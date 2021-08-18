289da0844aba7889dbfb5d093cda33f5df56f289
Darwin <38464772+darwinknowless@users.noreply.github.com>
2021-08-18 19:07:18 +0700

    create model, controller, connection. get gin gonic

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"movie-review/controller"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	var db controller.DB
+	r := gin.Default()
+
+	// movie
+	r.GET("/movie", db.GetAllMovie)
+	r.POST("/movie", db.PostMovie)
+
+	// author
+
+	r.Run(":9000")
 }
